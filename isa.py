2e7f8d30e3f00455a7b95792236c5ad32a2166f4
Oleg Shipulin <olegshipu95@gmail.com>
2024-02-29 00:07:26 +0200

    write isa and translator

--- isa.py
+++ isa.py
@@ -1,8 +1,63 @@
+from __future__ import annotations
+
+import json
 from enum import Enum
 
 
-def write_code(filename: str, code: list[dict]):
-    pass
+class OpcodeParamType(str, Enum):
+    CONST = "const"
+    ADDR = "addr"
+    UNDEFINED = "undefined"
+    ADDR_REL = "addr_rel"
+
+
+class OpcodeParam:
+    def __init__(self, param_type: OpcodeParamType, value: any):
+        self.param_type = param_type
+        self.value = value
+
+    def __str__(self):
+        return f"({self.param_type}, {self.value})"
+
+
+class OpcodeType(str, Enum):
+    DROP = "drop"  # 1
+    MUL = "mul"
+    DIV = "div"
+    SUB = "sub"
+    ADD = "add"
+    MOD = "mod"
+    SWAP = "swap"  # ?
+    OVER = "over"
+    DUP = "dup"  # 1
+    EQ = "eq"
+    GR = "gr"
+    LS = "ls"
+    DI = "di"  # 0
+    EI = "ei"  # 0
+    OMIT = "omit"  # 1
+    READ = "read"  # 0 -> 1
+
+    # not used in source code, compile-generated
+    STORE = "store"
+    LOAD = "load"
+    PUSH = "push"
+    RPOP = "rpop"  # move from return stack to data stack
+    POP = "pop"  # move from data stack to return stack
+    JMP = "jmp"
+    ZJMP = "zjmp"
+    CALL = "call"
+    RET = "ret"
+    HALT = "halt"
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Opcode:
+    def __init__(self, opcode_type: OpcodeType, params: list[OpcodeParam]):
+        self.opcode_type = opcode_type
+        self.params = params
 
 
 class TermType(Enum):
@@ -45,3 +100,16 @@ class TermType(Enum):
         STRING,
         ENTRYPOINT,
     ) = range(35)
+
+
+def write_code(filename: str, code: list[dict]):
+    with open(filename, "w", encoding="utf-8") as file:
+        buf = []
+        for instr in code:
+            buf.append(json.dumps(instr))
+        file.write("[" + ",\n ".join(buf) + "]")
+
+
+def read_code(source_path: str) -> list:
+    with open(source_path, encoding="utf-8") as file:
+        return json.loads(file.read())
